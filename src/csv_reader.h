bf680ab5deffd546d62ff90954f2e3367f285cec
Julian Albarracin <jalbarracing@unal.edu.co>
2026-03-08 13:51:55 -0500

    feat: add csv_reader.c, implementation of header

--- src/csv_reader.h
+++ src/csv_reader.h
@@ -1,6 +1,6 @@
 #ifndef CSV_READER_H
 #define CSV_READER_H
-
+#include <stdio.h>
 typedef struct row{
     int id;
     char title[512];
@@ -8,12 +8,15 @@ typedef struct row{
     char date[16];
     char artist[2048];
     char url[64];
-    short streams;
+    long streams;
     char album[512];
     int duration;
     char explicito[8];
-} 
+} Row;
 
-
+FILE *open_csv(const char *filename);
+Row *read_csv(FILE *file, long offset);
+void print_row(Row *row);
+void close_csv(FILE *file);
 
 #endif
