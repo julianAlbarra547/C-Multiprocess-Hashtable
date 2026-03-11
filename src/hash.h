97e4de521f416fa9e1c52e4b28e668b0b3a2ba49
Julian Albarracin <jalbarracing@unal.edu.co>
2026-03-10 19:26:59 -0500

    feat: add hash.h with methods.

---
+++ src/hash.h
@@ -0,0 +1,23 @@
+#ifndef HASH_H
+#define HASH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define HASH_TABLE_SIZE 10000
+
+typedef struct hash_node{
+    char title[512];
+    char artist[2048];
+    long long offset;
+    struct hash_node *next;
+} Hash_node;
+
+int hash(const char *title);
+int insert_node(Hash_node **hash_table, char *title, char *artist, long long offset);
+long search_node(Hash_node **table, char *title, char *artist);
+void build_index(const char *csv_path, Hash_node **table);
+void save_index(Hash_node **table, const char *idx_path);
+void load_index(const char *idx_path, Hash_node **table);
+
+#endif
