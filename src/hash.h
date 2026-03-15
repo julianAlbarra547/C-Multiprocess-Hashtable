463be88df91cd48f558baccff695d0f2d2b4b352
Julian Albarracin <jalbarracing@unal.edu.co>
2026-03-15 13:49:23 -0500

    fix: modify structure of hash module

--- src/hash.h
+++ src/hash.h
@@ -5,20 +5,19 @@
 #include <stdlib.h>
 
 #define HASH_TABLE_SIZE 10000
+#define TABLE_IDX "spotify.idx"
+#define ENTRIES_BIN "spotify_entries.bin"
 
 typedef struct hash_node{
     char title[512];
     char artist[2048];
-    long long offset;
-    struct hash_node *next;
+    long offset;
+    long next_entry;
 } Hash_node;
 
 unsigned int hash(char *title);
-int create_hash_table(Hash_node **hash_table);
-int insert_node(Hash_node **hash_table, char *title, char *artist, long offset); 
-long search_node(Hash_node **table, char *title, char *artist);
-int build_index(const char *csv_path, Hash_node **table);
-int save_index(Hash_node **table, const char *idx_path);
-int load_index(const char *idx_path, Hash_node **table);
+int build_index(const char *csv_path, const char *idx_path, const char *entries_path);
+long search_node(long *table, FILE *entries, char *title, char *artist);
+int load_table(const char *idx_path, long *table);
 
 #endif
