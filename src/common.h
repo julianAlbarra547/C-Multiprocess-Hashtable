3e481b79188aa1c5ebcc4f9cdef752b809586aa9
Julian Albarracin <jalbarracing@unal.edu.co>
2026-03-16 18:13:09 -0500

    fix: reboot

---
+++ src/common.h
@@ -0,0 +1,27 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+
+#define FIFO_CLIENT_PATH "/tmp/client_fifo"
+#define FIFO_SERVER_PATH "/tmp/server_fifo"
+
+typedef struct row{
+    int id;
+    char title[512];
+    short rank;
+    char date[16];
+    char artist[2048];
+    char url[128];
+    long long streams;
+    char album[512];
+    double duration;
+    char explicito[8];
+} Row;
+
+typedef struct {
+    char title[512];
+    char author[1024];
+} Query;
+
+#endif
---
+++ src/ui_process.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include "common.h"
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+
+void print_menu(){                                       // Imprime el menu principal de opciones en pantalla.
+    printf("\n Bienvenido! Seleccione una opcion:\n");
+    printf("1) Buscar canción.\n");
+    printf("2) Agregar registro.\n");
+    printf("3) Salir\n");
+    printf("Elegir opcion: ");
+}
+
+int main(){
+
+    int fdwrite, fdread;
+    short option;
+    short start = 1;
+
+    fdwrite = open(FIFO_CLIENT_PATH, O_WRONLY);
+    if (fdwrite == -1) {
+        perror("Open write fifo");
+        return -1;
+    }
+
+    fdread = open(FIFO_SERVER_PATH, O_RDONLY);
+    if (fdread == -1) {
+        perror("Open read fifo");
+        return -1;
+    }
+
+    while(start){
+        print_menu();
+        scanf("%hd", &option);
+
+        switch (option){
+            case 1:
+                printf("Opcion 1 seleccionada.\n");
+                break;
+            case 2:
+                printf("Opcion 2 seleccionada.\n");
+                break;
+            case 3:
+                printf("Saliendo del programa...\n");
+                start = 0;
+                continue;
+            default:
+                printf("Opcion no valida. Intente nuevamente.\n");
+        }
+    }
+
+    close(fdwrite);
+    close(fdread);
+}
