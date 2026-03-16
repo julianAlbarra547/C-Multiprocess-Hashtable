e6ae06fdfc656628866dbe7b3dcabd3705c3d685
Julian Albarracin <jalbarracing@unal.edu.co>
2026-03-16 18:39:19 -0500

    feat: add option1 method

--- src/common.h
+++ src/common.h
@@ -5,20 +5,6 @@
 
 #define FIFO_CLIENT_PATH "/tmp/client_fifo"
 #define FIFO_SERVER_PATH "/tmp/server_fifo"
-
-typedef struct row{
-    int id;
-    char title[512];
-    short rank;
-    char date[16];
-    char artist[2048];
-    char url[128];
-    long long streams;
-    char album[512];
-    double duration;
-    char explicito[8];
-} Row;
-
 typedef struct {
     char title[512];
     char artist[1024];
