a00c6082786aced6c62059df69534e2863cfdda8
Julian Albarracin <jalbarracing@unal.edu.co>
2026-03-16 19:50:09 -0500

    feat: add utils module and finish ui_process

---
+++ src/utils.h
@@ -0,0 +1,15 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+void trim(char *text);
+int  valid_date(char *date);
+int  valid_explicit(char *explicito);
+int  valid_positive_int(char *buff);
+int  valid_positive_double(char *buff);
+
+#endif
